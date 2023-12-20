f1f31a1f9092f4106e98879bdae92369fa0f2ee7
Thijs <thijs0302@gmail.com>
2023-12-20 19:12:12 +0100

    save push

--- view/DeleteNounView.js
+++ view/DeleteNounView.js
@@ -1,5 +1,5 @@
 // DeleteUserView.js
-import { View ,Text } from 'react-native';
+import { View,Text } from 'react-native';
 // import { useUsersContext } from '../controller/UsersController';
 
 const DeleteUserView = () => {
